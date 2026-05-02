cc5aaf87c62fb05193b959a04e952529d049bd8a
Oskar <oskarsaarmaa@gmail.com>
2026-05-02 20:39:43 +0300

    Added salt configuration

---
+++ install_salt.sh
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Salt Bootstrap Script Migration Warning</h1>
+<p>
+    In October 2024, the Salt Project package repositories migrated.
+    
+    <strong>
+        Both bootstrap.saltproject.io and winbootstrap.saltproject.io
+        were set to be decomissioned by end of November 2024. An extension
+        was given to January 2025. This page is still hosted as a way to
+        inform and direct users to new locations.
+    </strong>
+</p>
+<p>
+    New download locations:
+
+    <ul>
+        <li><a href="https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.sh">https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.sh</a></li>
+        <li><a href="https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.sh.sha256">https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.sh.sha256</a></li>
+        <li><a href="https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.ps1">https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.ps1</a></li>
+        <li><a href="https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.ps1.sha256">https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.ps1.sha256</a></li>
+    </ul>
+</p>
+
+<h2>Resources</h2>
+
+<ul>
+    <li><a href="https://saltproject.io/blog/upcoming-bootstrap-decommission/">Upcoming bootstrap subdomain decommission</a></li>
+    <li><a href="https://saltproject.io/blog/post-migration-salt-project-faqs/">FAQs from Salt Project Repo Migration</a></li>
+    <li><a href="https://github.com/saltstack/salt-bootstrap">salt-bootstrap code repo</a></li>
+    <li><a href="https://docs.saltproject.io/salt/install-guide/en/latest/index.html">Salt Install Guide</a></li>
+</ul>
+
+</body>
+</html>
---
+++ srv/salt/grafana/init.sls
@@ -0,0 +1,3 @@
+install_grafana:
+     pkg.installed:
+       - name: grafana
